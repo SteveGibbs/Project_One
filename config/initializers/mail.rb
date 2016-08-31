82137029a503e6b0370b078b55e55faf15f9bccc
SteveGibbs <gibbsstephen@hotmail.com>
2016-08-31 12:06:33 +1000

    fixing syntax on mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,12 +3,12 @@ ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {
   :user_name => Rails.application.secrets.sendgrid_username,
   :password => Rails.application.secrets.sendgrid_password,
-  :domain => 'localhost',
+  :domain => Rails.application.secrets.domain,
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.delivery_method = :smtp_settings
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.default charset: 'utf-8'
