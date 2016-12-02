60a99baabbbe5bcb1669f5f6697279e341bf2a8d
SteveGibbs <gibbsstephen@hotmail.com>
2016-12-03 08:08:28 +1100

    adding slick carousel and content to individual country and city pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
+$('document').ready(function(){
+
+  // CAROUSEL SLIDER FROM SLICK
+    $('.autoplay').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      variableWidth: true,
+      autoplay: true,
+      autoplaySpeed: 3000
+    });
+
+    $('.slick-prev').html('<');
+    $('.slick-next').html('>');
+
+});
